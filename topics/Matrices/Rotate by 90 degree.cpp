70fc9dc7ed29e39c6a68846b9b9b86aab2c443f5
Kishan Periwal <klperiwal@gmail.com>
2024-03-25 17:19:25 +0530

    General Matrix Problems

--- topics/Matrices/Rotate by 90 degree.cpp
+++ topics/Matrices/Rotate by 90 degree.cpp
@@ -1,25 +1,30 @@
-Only Square Matrices:
+Code valid for only Square Matrices:
 
---> Rotate Anticlockwise:
-    void rotateanticlockwise(vector<vector<int>>& matrix) 
-    { 
-	int n=matrix.size();
-        for(int i=0;i<n;i++)
-            for(int j=0;j<i;j++)
-                swap(matrix[i][j],matrix[j][i]);
-                
-        for(int i=0;i<n/2;i++)
-            swap(matrix[i],matrix[n-1-i]);
-    } 
-
-
--->Rotate Clockwise:
-void rotate(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        for(int i=0;i<n;i++)
-            for(int j=0;j<i;j++)
-                swap(matrix[i][j],matrix[j][i]);
-
-        for(int i=0;i<n;i++)
-            reverse(matrix[i].begin(),matrix[i].end());   
+--> Anticlockwise Rotation:
+void rotateanticlockwise(vector<vector<int>>& matrix) 
+{ 
+    int n=matrix.size();
+    for(int i=0;i<n;i++)
+    {
+    	for(int j=0;j<i;j++)
+	   swap(matrix[i][j],matrix[j][i]);
     }
+	
+    for(int i=0;i<n/2;i++)
+    	swap(matrix[i],matrix[n-1-i]);
+} 
+
+
+--> Clockwise Rotation:
+void rotateclockwise(vector<vector<int>>& matrix) 
+{
+    int n=matrix.size();
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<i;j++)
+	   swap(matrix[i][j],matrix[j][i]);
+    }
+	
+    for(int i=0;i<n;i++)
+        reverse(matrix[i].begin(),matrix[i].end());   
+}
